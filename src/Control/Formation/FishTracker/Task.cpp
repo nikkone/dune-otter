53087d6f9ef1a847e7d37bb4d3b695633473508e
nikolai <nikolai@holmafjellet.net>
2023-04-18 11:59:52 +0200

    Added parsing and use of custom parameters in addition to various upgrades and bug fixes

--- src/Control/Formation/FishTracker/Task.cpp
+++ src/Control/Formation/FishTracker/Task.cpp
@@ -46,8 +46,6 @@ namespace Control
         //! Sync Period;
         double ref_send_interval;
         //!
-        bool rotate_formation;
-        //!
         double formation_rotation_step;
       };
 
@@ -62,19 +60,29 @@ namespace Control
         IMC::RemoteSensorInfo m_last_rs_msg;
 
         IMC::otterFormation m_last_of_msg;
+        //! The rotation state of the entire formation
+        double m_formation_rotate_rad;
+        //! Initialized from args, may be overwritten by custom parameter
+        double m_formation_rotation_step_rad;
+        //! Initialized from args, may be overwritten by custom parameter
+        double m_minTagInterval;
+        //! TODO: Not sure if needed
+        double m_maxTagInterval;
+        //! The timeout used in the FollowReference maneuvers sent by this task
+        double m_timeout;
+        //! Initialized from args, may be overwritten by custom parameter
+        double m_FollowRefInterval;
         //!
-        std::vector<uint16_t> participant_ids;
-        std::vector<IMC::Reference> participant_refs;
-
-        double m_formation_rotate_radians;
-        bool m_rotate_formation;
-        double m_formation_rotation_step;
+        bool m_formation_started;
+        //! 
+        double m_formation_radius;
+        //! vehicleid - Lat(rad), Lon(rad), LastReference, time since last position update
+        std::map<uint16_t, std::tuple<fp64_t, fp64_t, IMC::Reference, DUNE::Time::Delta>> m_participants; 
 
         Task(const std::string &name, Tasks::Context &ctx) : 
         DUNE::Tasks::Task(name, ctx),
-        m_formation_rotate_radians(0.0),
-        m_rotate_formation(true),
-        m_formation_rotation_step(M_PI/2) // 90deg
+        m_formation_rotate_rad(0.0),
+        m_formation_rotation_step_rad(M_PI/2) // 90deg
         {
           param("FishTag min interval", m_args.fishtag_min_interval)
               .units(Units::Second)
@@ -88,10 +96,6 @@ namespace Control
               .minimumValue("0.0")
               .description("Period between sync messages");
 
-          param("Rotate Formation", m_args.rotate_formation)
-              .defaultValue("False")
-              .description("Period between sync messages");
-
           param("Rotation Step", m_args.formation_rotation_step)
               .units(Units::Radian)
               .defaultValue("0.0")
@@ -99,23 +103,26 @@ namespace Control
               .maximumValue("6.284") // ~2PI
               .description("Period between sync messages");
 
+          bind<IMC::Abort>(this);
+          bind<IMC::Announce>(this);
           bind<IMC::otterFormation>(this);
           bind<IMC::RemoteSensorInfo>(this);
-          bind<IMC::Abort>(this);
           //bind<IMC::TBRFishTag>(this);
         }
         //! Update internal state with new parameter values.
         void
         onUpdateParameters(void)
         {
-          if (paramChanged(m_args.fishtag_min_interval))
+          if(paramChanged(m_args.fishtag_min_interval))
             m_last_tag_timer.setTop(m_args.fishtag_min_interval);
-          if (paramChanged(m_args.ref_send_interval))
+          if(paramChanged(m_args.ref_send_interval))
             m_ref_send_timer.setTop(m_args.ref_send_interval);
-          if (paramChanged(m_args.rotate_formation))
-            m_rotate_formation = m_args.rotate_formation;
-          if (paramChanged(m_args.formation_rotation_step))
-            m_formation_rotation_step = m_args.formation_rotation_step;
+          if(paramChanged(m_args.formation_rotation_step))
+            m_formation_rotation_step_rad = m_args.formation_rotation_step;
+          if(paramChanged(m_args.fishtag_min_interval))
+            m_minTagInterval =  m_args.fishtag_min_interval;
+          if(paramChanged(m_args.ref_send_interval))
+            m_FollowRefInterval = m_args.ref_send_interval;
         }
         
         void
@@ -131,13 +138,7 @@ namespace Control
         void
         onResourceInitialization(void)
         {
-          // DIRTY TEMP SOLUTION
-          participant_ids.push_back(getSystemId());
-          participant_ids.push_back(0x2811);
-          participant_ids.push_back(0x2812);
-          participant_refs.push_back(IMC::Reference());
-          participant_refs.push_back(IMC::Reference());
-          participant_refs.push_back(IMC::Reference());
+
         }
 
         void dispatchToVehicle(uint16_t vehicle, IMC::Message *msg)
@@ -157,7 +158,7 @@ namespace Control
           man.control_ent = getEntityId();
           man.control_src = getSystemId();
           man.altitude_interval = 0;
-          man.timeout = 10;
+          man.timeout = m_timeout;
 
           IMC::PlanSpecification spec;
 
@@ -172,13 +173,14 @@ namespace Control
           startPlan.request_id = 0;
           startPlan.flags = 0;
           startPlan.setDestination(m_ctx.resolver.id());
-          for (const auto participant_id : participant_ids)
+          for (const auto &participant : m_participants)
           {
-            dispatchToVehicle(participant_id, &startPlan);
+            dispatchToVehicle(participant.first, &startPlan);
           }
           m_last_tag_timer.reset();
           m_ref_send_timer.reset();
-          m_formation_rotate_radians = 0;
+          m_formation_rotate_rad = 0;
+          m_formation_started = false;
           setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
         }
 
@@ -191,45 +193,12 @@ namespace Control
           stopPlan.type = IMC::PlanControl::PC_REQUEST;
           stopPlan.op = IMC::PlanControl::PC_STOP;
           stopPlan.plan_id = "fishSearch_plan";
-          for (const auto participant_id : participant_ids)
+          for (const auto &participant : m_participants)
           {
-            dispatchToVehicle(participant_id, &stopPlan);
+            dispatchToVehicle(participant.first, &stopPlan);
           }
           setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_IDLE);
-        }
-
-        void consume(const IMC::otterFormation* msg) {
-          inf("Got otterFormation following %s", msg->target.c_str());
-          m_last_of_msg = *msg;
-          switch(msg->msg_type) {
-            case IMC::otterFormation::MessageTypeEnum::T_start:
-            if(!isActive()) {
-              requestActivation();
-            }
-            break;
-            case IMC::otterFormation::MessageTypeEnum::T_stop:
-            if(isActive()) {
-              requestDeactivation();
-            }
-            break;
-            case IMC::otterFormation::MessageTypeEnum::T_param_change:
-            inf("Updating controller with parameter changes");
-            //TODO;
-            break;
-            default:
-            err("Unsupported msg_type, doing nothing.");
-          }
-        }
-
-        void consume(const IMC::RemoteSensorInfo *msg)
-        {
-          if(m_last_of_msg.target == msg->id) {
-            m_last_rs_msg = *msg;
-            m_last_tag_timer.reset();
-            if(m_rotate_formation) {
-              m_formation_rotate_radians += m_formation_rotation_step;
-            }
-          }
+          m_formation_started = false;
         }
 
         void consume(const IMC::Abort* msg)
@@ -241,18 +210,106 @@ namespace Control
           requestDeactivation();
         }
 
-        std::vector<std::pair<double, double>> generateRegularPolygonVertices(double x0, double y0, double radius, int numVertices, int rotAngle)
+        void consume(const IMC::Announce* msg)
         {
-          std::vector<std::pair<double, double>> retval;
-          double angle = 2 * M_PI / numVertices; // Calculate the angle between each vertex
-          for (int i = 0; i < numVertices; i++)
-          {
-            double x = x0 + radius * std::cos(i * angle + rotAngle); // Calculate the x-coordinate of the vertex
-            double y = y0 + radius * std::sin(i * angle + rotAngle); // Calculate the y-coordinate of the vertex
-            //std::cout << "(" << x << "," << y << ")" << std::endl; // Output the vertex coordinates
-            retval.push_back(std::pair<double, double>(x, y));
+            if(msg->getSource() != getSystemId()) {
+              if(!m_participants.empty()) {
+                auto current = m_participants.find(msg->getSource());
+                if( current != m_participants.end()) {
+                  // Add position to list, then check for collisions in EstimatedState
+                  std::get<0>(current->second) = msg->lat;
+                  std::get<1>(current->second) = msg->lon; 
+                  std::get<3>(current->second).reset(); 
+                }
+              }
           }
-          return retval;
+        }
+
+        void consume(const IMC::otterFormation* msg) {
+          inf("Got otterFormation following %s", msg->target.c_str());
+          
+          switch(msg->msg_type) {
+            case IMC::otterFormation::MessageTypeEnum::T_start:
+              m_last_of_msg = *msg;
+              m_formation_radius = msg->minradius;
+              if(!isActive()) {
+
+                // Add vehicles
+                std::vector<std::string> parts;
+                String::split(msg->participants, ",", parts);
+                m_participants.clear();
+                for (const auto &participant_name : parts) {
+                  spew("Vehicle added: %d", resolveSystemName(participant_name));
+                  m_participants[resolveSystemName(participant_name)] = std::tuple<fp64_t, fp64_t, IMC::Reference, DUNE::Time::Delta>{0.0,0.0, IMC::Reference(),DUNE::Time::Delta()};
+                }
+                parseCustomParameters(msg->custom);
+                requestActivation();
+              }
+              break;
+            case IMC::otterFormation::MessageTypeEnum::T_stop:
+              if(isActive()) {
+                requestDeactivation();
+              }
+              break;
+            case IMC::otterFormation::MessageTypeEnum::T_param_change:
+              inf("Updating controller with parameter changes");
+              parseCustomParameters(msg->custom);
+              m_formation_radius = msg->minradius;
+              //TODO;
+              break;
+            default:
+            err("Unsupported msg_type, doing nothing.");
+          }
+        }
+
+        void consume(const IMC::RemoteSensorInfo *msg)
+        {
+          if(isActive()) {
+            if(m_last_of_msg.target == msg->id) {
+              m_formation_started = true;
+              m_last_rs_msg = *msg;
+              m_last_tag_timer.reset();
+              if(m_formation_rotation_step_rad > 0.0) {
+                spew("Rotating %f", m_formation_rotate_rad);
+                m_formation_rotate_rad += m_formation_rotation_step_rad;
+              }
+            }
+          }
+        }
+
+        void parseCustomParameters(std::string parameters) {
+          DUNE::Utils::TupleList custom = DUNE::Utils::TupleList(parameters);
+          // Parse parameters
+          double rotation_dist = custom.get("r", double(-1.0));
+          double minTagInterval = custom.get("i", double(-1.0));
+          double maxTagInterval = custom.get("x", double(-1.0));
+          double timeout = custom.get("t", double(-1.0));
+          double FollowRefInterval = custom.get("f", double(-1.0));
+          // Validate an update parameters
+          if(rotation_dist < 0.0) {
+            war("No/Wrong 'r', using rotation_dist from task arguments");
+          } else {
+            m_formation_rotation_step_rad = rotation_dist;
+          } if(minTagInterval < 0.0) {
+            war("No/Wrong 'i', using minTagInterval from task arguments");
+          } else {
+            m_minTagInterval = minTagInterval;
+          } if(maxTagInterval < 0.0) {
+            war("No/Wrong 'x', using maxTagInterval from task arguments");
+          } else {
+            m_maxTagInterval = maxTagInterval;
+          } if(timeout < 0.0) {
+            war("No/Wrong 't', using timeout from task arguments");
+          } else {
+            m_timeout = timeout;
+          } if(FollowRefInterval < 0.0) {
+            war("No/Wrong 'f', using FollowRefInterval from task arguments");
+          } else {
+            m_FollowRefInterval = FollowRefInterval;
+            m_ref_send_timer.setTop(m_FollowRefInterval);
+          }
+
+          debug("r=%f, i=%f, x=%f, t=%f, f=%f", rotation_dist, minTagInterval, maxTagInterval, timeout, FollowRefInterval);
         }
 
         std::vector<std::pair<double, double>> generateTrackingFormation(double x0, double y0, double radius, unsigned numVertices, double rotAngle)
@@ -291,27 +348,35 @@ namespace Control
 
           return retval;
         }
+        // TODO
+        void formationAllocator() {
+
+        }
 
         void updateReference() {
           DUNE::IMC::DesiredSpeed m_dsp;
-          m_dsp.value = 1.0;
-          m_dsp.speed_units = IMC::SUNITS_METERS_PS;
-          std::vector<std::pair<double, double>> pos = generateTrackingFormation(0,0, 60, participant_refs.size(), m_formation_rotate_radians);
-          inf("Participants: %ld", participant_refs.size());
+          m_dsp.value = m_last_of_msg.maxspeed;
+          m_dsp.speed_units = m_last_of_msg.speed_units;
+          std::vector<std::pair<double, double>> pos = generateTrackingFormation(0,0, m_formation_radius, m_participants.size(), m_formation_rotate_rad);
+          inf("Participants, rotstate %ld: %f", m_participants.size(), m_formation_rotate_rad);
           double lat,lon;
-          for (unsigned i = 0;i<participant_refs.size();i++)
-          {
+          unsigned i=0;
+          for (auto &participant : m_participants) {
             lon = m_last_rs_msg.lon;
             lat = m_last_rs_msg.lat;
 
             DUNE::Coordinates::WGS84::displace(pos[i].first, pos[i].second, &lat, &lon);
-            participant_refs[i].setDestination(participant_ids[i]);
-            participant_refs[i].lon = lon;
-            participant_refs[i].lat = lat;
-            spew("%d - %f, %f", i, DUNE::Math::Angles::degrees(participant_refs[i].lon), DUNE::Math::Angles::degrees(participant_refs[i].lat));
-            participant_refs[i].speed.set(m_dsp);
-            participant_refs[i].flags = IMC::Reference::FlagsBits::FLAG_LOCATION | IMC::Reference::FlagsBits::FLAG_SPEED;
-            dispatch(participant_refs[i]);
+            std::get<2>(participant.second).setDestination(participant.first);
+            std::get<2>(participant.second).lon = lon;
+            std::get<2>(participant.second).lat = lat;
+            std::get<2>(participant.second).speed.set(m_dsp);
+            std::get<2>(participant.second).flags = IMC::Reference::FlagsBits::FLAG_LOCATION | IMC::Reference::FlagsBits::FLAG_SPEED;
+
+            spew("%d - %f, %f", i, DUNE::Math::Angles::degrees(std::get<2>(participant.second).lon), DUNE::Math::Angles::degrees(std::get<2>(participant.second).lat));
+            dispatch(std::get<2>(participant.second));
+
+            inf("Participant: %d, Location: %f, %f", participant.first, std::get<0>(participant.second), std::get<1>(participant.second));
+            i++;
           }
         }
 
@@ -326,8 +391,11 @@ namespace Control
               if(m_ref_send_timer.overflow()) {
                 if(!m_last_tag_timer.overflow())
                 {
-                  spew("Ref Update");
-                  updateReference();       
+                  if(m_formation_started) {
+                    spew("Ref Update");
+                    updateReference(); 
+                  }
+      
                 } else {
                   // Also threashold to keep standing still
                   // Check if over threashold, then requestDeactivation
