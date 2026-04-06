8784726bb6be31b54fe7d6dfd15b5880a93da386
nikkone <nikolai@holmafjellet.net>
2026-04-06 06:59:03 +0200

    Update LeastSquares.cpp
    Fixed ln to log error in ambiguity solvr of xkf

--- vendor/libraries/FishTagEstimators/XKFVelocity/LeastSquares.cpp
+++ vendor/libraries/FishTagEstimators/XKFVelocity/LeastSquares.cpp
@@ -167,7 +167,7 @@ namespace FishTagEstimators
           T SNRerror1 = 0;
           for(auto it: tagDetections) {
             T d1 = (xHat1 - Eigen::Matrix<T, 3, 1>(it.N, it.E, it.D)).norm();
-            T SNRest = B_fit - k_fit*std::log(d1) - a_fit*d1;
+            T SNRest = B_fit - k_fit*std::log10(d1) - a_fit*d1;
             SNRerror1 = SNRerror1 + std::pow(it.snr - SNRest, 2);
             std::cout << it.serial_no << " - " << it.millis << " - " << d1 << " - " << int(it.snr) << " - " << SNRest << " - " << SNRerror1 << std::endl;
             if(d1 < dist) {
@@ -184,7 +184,7 @@ namespace FishTagEstimators
           T SNRerror2 = 0;
           for(auto it: tagDetections) {
             T d1 = (xHat2 - Eigen::Matrix<T, 3, 1>(it.N, it.E, it.D)).norm();
-            T SNRest = B_fit - k_fit*std::log(d1) - a_fit*d1;
+            T SNRest = B_fit - k_fit*std::log10(d1) - a_fit*d1;
             SNRerror2 = SNRerror2 + std::pow(it.snr - SNRest, 2);
             std::cout << it.serial_no << " - " << it.millis << " - " << d1 << " - " << int(it.snr) << " - " << SNRest << " - " << SNRerror2 << std::endl;
             if(d1 < dist) {
